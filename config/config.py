2b38e337849f9e95a08d8366c28f572eda6df340
Kassim darlzzz <117517237+Kassim-the-devil@users.noreply.github.com>
2022-12-02 01:14:27 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001821418353"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("Darlzzz music")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 
 # Your User ID.
 OWNER_ID = list(
